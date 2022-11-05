06093c7159eab472a60b9fcd39b264fd3eeb9b15
Daichi Nishida <verhichi@hotmail.com>
2022-11-05 21:31:14 +0900

    chore: add lint and monaco editor package

--- src/pages/Monaco/Monaco.tsx
+++ src/pages/Monaco/Monaco.tsx
@@ -1,1 +1,27 @@
-export const Monaco = () => <div>Monaco</div>
+import * as monaco from 'monaco-editor/esm/vs/editor/editor.api'
+import { useEffect, useRef, useState } from 'react'
+
+import styles from './Monaco.module.css'
+
+export const Monaco = () => {
+  const [editor, setEditor] =
+    useState<monaco.editor.IStandaloneCodeEditor | null>(null)
+  const monacoEl = useRef(null)
+
+  useEffect(() => {
+    if (monacoEl.current != null && editor == null) {
+      setEditor(
+        monaco.editor.create(monacoEl.current, {
+          value: ['function x() {', '\tconsole.log("Hello world!");', '}'].join(
+            '\n',
+          ),
+          language: 'typescript',
+        }),
+      )
+    }
+
+    return () => editor?.dispose()
+  }, [monacoEl.current])
+
+  return <div className={styles.editor} ref={monacoEl}></div>
+}
